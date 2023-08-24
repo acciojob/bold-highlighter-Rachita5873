24d1c956a777e1e8712350a2dcc1395b6c73ef76
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-08-24 08:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+ var boldItems = document.getElementsByTagName('strong');
 function highlight() {
-    //Write your code here
-
-
+    // Get all the strong elements
+    
+    // Loop through the array and change the color of each element to green
+    for (var i = 0; i < boldItems.length; i++) {
+        boldItems[i].style.color = 'green';
+    }
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    // Get all the strong elements
+    // var boldItems = document.getElementsByTagName('strong'); 
+    // Loop through the array and change the color of each element back to black
+    for (var i = 0; i < boldItems.length; i++) {
+        boldItems[i].style.color = 'black';
+    }
 }
